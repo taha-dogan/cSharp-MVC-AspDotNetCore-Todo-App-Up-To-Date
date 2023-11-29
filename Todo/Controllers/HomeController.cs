a6b777e8a266982e8d8b595c11a8031a18617a8b
flakysob <flakysob@gmail.com>
2023-11-29 20:44:08 +0300

    Finished with Some Front-End Issues

--- Todo/Controllers/HomeController.cs
+++ Todo/Controllers/HomeController.cs
@@ -84,19 +84,97 @@ public class HomeController : Controller
         return Redirect("https://localhost:7159/");
     }
 
-     [HttpPost]
-        public JsonResult Delete(int id)
+
+
+
+
+    [HttpPost]
+    public JsonResult Delete(int id)
+    {
+        using (SqliteConnection con = new SqliteConnection("Data Source=db.sqlite"))
         {
-            using (SqliteConnection con = new SqliteConnection("Data Source=db.sqlite"))
+            using (var tableCmd = con.CreateCommand())
             {
-                using (var tableCmd = con.CreateCommand())
+                con.Open();
+                tableCmd.CommandText = $"DELETE from todo WHERE Id = '{id}'";
+                tableCmd.ExecuteNonQuery();
+            }
+        }
+
+            return Json(new {});
+    }
+
+
+
+
+
+
+    [HttpGet]
+    public JsonResult PopulateForm(int id)
+    {
+        var todo = GetById(id);
+        return Json(todo);
+    }
+
+
+
+
+
+    internal TodoItem GetById(int id)
+    {
+        TodoItem todo = new();
+
+        using(var connection = new SqliteConnection("Data Source = db.sqlite"))
+        {
+            using (var tableCmd = connection.CreateCommand())
+            {
+                connection.Open();
+                tableCmd.CommandText = $"SELECT * FROM todo WHERE Id = {id}";
+
+                using(var reader = tableCmd.ExecuteReader())
                 {
-                    con.Open();
-                    tableCmd.CommandText = $"DELETE from todo WHERE Id = '{id}'";
+                    if (reader.HasRows)
+                    {
+                        reader.Read();
+                        todo.Id = reader.GetInt32(0);
+                        todo.Name = reader.GetString(1);
+                    }
+                    else
+                    {
+                        return todo;
+                    }
+                };
+            }
+        }
+        return todo;
+    }
+
+
+
+
+
+
+
+    public RedirectResult Update(TodoItem todo)
+    {
+        using (SqliteConnection con = new SqliteConnection("Data Source=db.sqlite"))
+        {
+            using (var tableCmd = con.CreateCommand())
+            {
+                con.Open();
+                tableCmd.CommandText = $"UPDATE todo SET name = '{todo.Name}' WHERE Id = '{todo.Id}'";
+
+                try
+                {
                     tableCmd.ExecuteNonQuery();
                 }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
-
-             return Json(new {});
         }
+
+        return Redirect("https://localhost:7159/");
+    }
 }
--- Todo/wwwroot/js/site.js
+++ Todo/wwwroot/js/site.js
@@ -12,16 +12,16 @@
     });
 }
 
-function populateForm(i)
-{
+function populateForm(i) {
+
     $.ajax({
         url: 'Home/PopulateForm',
         type: 'GET',
         data: {
-            id : i
+            id: i
         },
         dataType: 'json',
-        success: function(repsonse){
+        success: function (response) {
             $("#Todo_Name").val(response.name);
             $("#Todo_Id").val(response.id);
             $("#form-button").val("Update Todo");
