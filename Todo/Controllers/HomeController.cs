f7dbdd04b64f3e76cb353008f7e242d5d346c952
flakysob <flakysob@gmail.com>
2023-11-29 11:07:14 +0300

    first commit

---
+++ Todo/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using Todo.Models;
+
+namespace Todo.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+    public void Insert(TodoItem todo) //TodoItem: Id ve Name proplarını içeren bir sınıf
+    {
+        using(SqliteConnection con = new SqliteConnection("Data Source=db.sqlite"))
+        {
+            using(var tableCmd = con.CreateCommand())
+            {
+                con.Open();
+                tableCmd.CommandText = $"INSERT INTO todo (name) VALUES ('{todo.Name}')";
+                try
+                {
+                    tableCmd.ExecuteNonQuery();
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Todo/Models/TodoItem.cs
@@ -0,0 +1,7 @@
+namespace Todo.Models;
+
+public class TodoItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
